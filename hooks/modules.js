935d57dcd0edc785e6746f3f84ea047ccb3e77c3
VormerICT <94116310+VormerICT@users.noreply.github.com>
2023-03-22 19:25:07 +0100

    Create modules.js

---
+++ hooks/modules.js
@@ -0,0 +1,19 @@
+module.exports = {
+   getPlatformConfigurationFile: function(context) {
+      var androidPlatformDir = path.join(context.opts.projectRoot,'platforms', 'android');
+      var oldConfigXMLLocation = path.join(androidPlatformDir, 'res', 'xml', 'config.xml');
+      var newConfigXMLLocation = path.join(androidPlatformDir, 'app', 'src', 'main', 'res', 'xml', 'config.xml');
+      var platformConfigurationFile;
+      
+      if (fs.existsSync(newConfigXMLLocation)) {
+        // cordova-android >= 7.0.0
+        platformConfigurationFile = newConfigXMLLocation;
+            console.log("config.xml found at " + newConfigXMLLocation);
+      } else {
+        // cordova-android < 7.0.0
+            console.log("config.xml found at " + oldConfigXMLLocation);
+        platformConfigurationFile = oldConfigXMLLocation;
+      }
+      return platformConfigurationFile;
+   }
+}
