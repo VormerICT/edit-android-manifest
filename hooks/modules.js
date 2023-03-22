655f87588905efe45da2d70719f1cb9062245e3d
VormerICT <94116310+VormerICT@users.noreply.github.com>
2023-03-22 20:49:59 +0100

    Update modules.js

--- hooks/modules.js
+++ hooks/modules.js
@@ -1,5 +1,7 @@
+const fs = require("fs"), path = require("path");
+
 module.exports = {
-   const fs = require("fs"), path = require("path");
+   
    getPlatformConfigurationFile: function(context) {
       var androidPlatformDir = path.join(context.opts.projectRoot,'platforms', 'android');
       var oldConfigXMLLocation = path.join(androidPlatformDir, 'res', 'xml', 'config.xml');
